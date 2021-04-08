b3b341f3ba1696f0ef2dc35b6f25d61b9fb814b3
CatSheiles <cathysheiles@gmail.com>
2021-04-08 12:11:17 +1000

    added server integration

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//A simple chat where users can message together
+//Wookie wrote a simple server that accepts and send messages to all connected
+//shows who messaged, date/time, message
+//can connect other ip's via http requests so you can have as many users as you want
+
 let messagebox = document.querySelector ("textarea");
 let button = document.querySelector ("button");
 let messages = document.querySelector ("ul");
@@ -7,21 +12,29 @@ nameBox.value = localStorage.getItem("username"); //saving users name
 
 function addMessageToList(messageObject)
 {
-    let newMessage = document.createElement("li");
+    messages.innerHTML = "";
+    messageObject.forEach(message => {
+        let newMessage = document.createElement("li");
 
-    newMessage.innerHTML = 
-    messageObject.name + "<br>" +
-    new Date(messageObject.date) +
-     ": <br>" + messageObject.message;
+        newMessage.innerHTML = 
+        message.name + "<br>" +
+        new Date(message.date) +
+        ": <br>" + message.message;
 
-
-    messages.appendChild(newMessage);
+        messages.appendChild(newMessage);
+    });
 }
 
-function getMessageFromSomeoneElse()
+function getMessageFromSomeoneElse(res)
 {
-    addMessageToList(whatYouRecievedFromServer);
+    addMessageToList(JSON.parse(res.target.response));
 }
+
+//Wookie test server
+var server = new XMLHttpRequest();
+server.addEventListener("load", getMessageFromSomeoneElse);
+server.open("GET", "http://192.168.0.125/messages");
+server.send();
 
 //add new message in input box, send and clear box
 button.onclick = () =>{
@@ -30,10 +43,11 @@ button.onclick = () =>{
         message: messagebox.value,
         date: Date.now()
     };
-    addMessageToList(messageObject)
+    addMessageToList([messageObject])
     messagebox.value = "";
 
     localStorage.setItem("username", nameBox.value); //saving users name
 
-    //sendToSocket(messageObject); didn't use socket for this example only local
+    server.open("POST", "http://192.168.0.125/send");
+    server.send(JSON.stringify(messageObject));
 }
